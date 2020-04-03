d8b8379f9fae015e5a315fe03a89a367adc729b5
zhouwenliang <762180450@qq.com>
2020-04-03 18:05:22 +0800

    更新快手新域名解析方法

--- Controllers/KuaiShouController.cs
+++ Controllers/KuaiShouController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -15,10 +18,28 @@ namespace ChouYuc_Api.Controllers
     {
         public Object Get(string url)
         {
-            var sig = HttpHelper.StringToMD5Hash("client_key=56c3713cshareText=" + url + System.Text.Encoding.ASCII.GetString(new byte[] { 50, 51, 99, 97, 97, 98, 48, 48, 51, 53, 54, 99 }));
-            var data = "client_key=56c3713c&shareText=" + url + "&sig=" + sig;
-            var result = HttpHelper.HttpGet("http://api.gifshow.com/rest/n/tokenShare/info/byText?" + data);
-            return result;
+            //快手新域名 kuaishou.com
+            if(url.ToLower().Contains("kuaishou.com"))
+            {
+                WebHeaderCollection header = new WebHeaderCollection();
+                header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36");
+                var html_302 = HttpHelper.HttpGet(url, header, false);
+                html_302 = HttpHelper.HttpGet(html_302, header, false);
+                var html = HttpHelper.HttpGet("https:"+html_302, header, false);
+                var resUrl = Regex.Match(html, "\"playUrl\":\"(.*?).mp4").Value.Replace("\"playUrl\":\"", "");
+                return resUrl.Replace("\\u002F","\\");
+            }
+            else
+            {
+                var sig = HttpHelper.StringToMD5Hash("client_key=56c3713cshareText=" + url + System.Text.Encoding.ASCII.GetString(new byte[] { 50, 51, 99, 97, 97, 98, 48, 48, 51, 53, 54, 99 }));
+                var data = "client_key=56c3713c&shareText=" + url + "&sig=" + sig;
+                var result = HttpHelper.HttpGet("http://api.gifshow.com/rest/n/tokenShare/info/byText?" + data);
+                return result;
+            }
+
+
+
+           
         }
     }
 }
