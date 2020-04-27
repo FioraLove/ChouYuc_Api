4656b6f7ec83f02e05cd3d51d441625725ae2ace
zhouwenliang <762180450@qq.com>
2020-04-27 09:54:50 +0800

    抖音返回地址有水印处理方法

--- Controllers/DouYinController.cs
+++ Controllers/DouYinController.cs
@@ -21,6 +21,9 @@ namespace ChouYuc_Api.Controllers
             var html_302 = HttpHelper.HttpGet(url);
             var dytk = Regex.Match(Regex.Match(html_302, "dytk(.*?)}").Value, "\\w+(?=\")").Value;
             var result = HttpHelper.HttpGet("https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=" + vid + "&dytk=" + dytk);
+            //返回地址已是有水印地址
+            //例如：https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200ff40000bp30umcttc5i66m6n4vg&ratio=720p&line=0
+            //只需要把vm去掉即可 就是无水印地址了 https://aweme.snssdk.com/aweme/v1/play/?video_id=v0200ff40000bp30umcttc5i66m6n4vg&ratio=720p&line=0
             return result;
         }
     }
